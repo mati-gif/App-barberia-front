197cfe72d50c2f1428268072be05b5132b1c32d7
mati-gif <matiassoria7700@gmail.com>
2025-01-02 18:36:35 -0300

    añadiendo lños servicios

--- src/components/ListServices.jsx
+++ src/components/ListServices.jsx
@@ -2,9 +2,72 @@ import React, { useEffect } from 'react'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 function ListServices() {
-        useEffect(() => {
-            AOS.init();
-        }, []);
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
+    const servicios = [
+        {
+            name: "ROYAL MASK",
+            img:"https://thespotbarbershop.com/wp-content/uploads/2023/05/facial-wax-on-mans-face.jpg",
+            price: "$18.00",
+            description: "PURIFIES THE SKIN BY DISSOLVING AND DISLODGING THE BUILDUP OF IMPURITIES"
+        },
+        {
+            name:"CLASSIC HAIRCUT",
+            img:"https://thespotbarbershop.com/wp-content/uploads/2023/06/ClassicHaircut-512x768.jpg",
+            price: "$25.00",
+            description:"CLASSIC STYLE HAIRCUT HALF GUARD AND UP"
+        },
+        {
+            name:"SKIN / BALD FADE",
+            img:"https://thespotbarbershop.com/wp-content/uploads/2023/06/Skin-Bald-Fade.jpg",
+            price: "$30.00",
+            description:"MODERN STYLE HAIRCUT ZERO GUARD WITH BLADE FINISH"
+        },
+        {
+            name:"EDGE-UP/LINE-UP",
+            img:"https://thespotbarbershop.com/wp-content/uploads/2023/06/EdgeUp-LineUp.jpg",
+            price: "$23.00",
+            description:"OUTLINE REFRESH RAZOR DETAILING"
+        },        
+        {
+            name:"ROYAL SHAVE",
+            img:"https://thespotbarbershop.com/wp-content/uploads/2023/06/RoyalShave.jpg",
+            price: "$32.00",
+            description:"TRADITIONAL RAZOR SHAVE WITH HOT TOWEL & LATHER"
+        },        
+        {
+            name:"REGULAR BEARD",
+            img:"https://thespotbarbershop.com/wp-content/uploads/2023/06/RegularBeard.jpg",
+            price: "$25.00",
+            description:"BEARD SERVICE - TRIMMING SCULPTING - BLADE EDGING"
+        },       
+        {
+            name:"WAX",
+            img:"https://thespotbarbershop.com/wp-content/uploads/2023/05/man-with-hairwax-on-nose-and-ear.jpg",
+            price: "$18.00",
+            description:"NOSE WAX | EAR WAX | EYEBROW WAX EACH SERVICE CHARGED SEPARATELY"
+        },        
+        {
+            name:"EYEBROW DETAILING",
+            img:"https://thespotbarbershop.com/wp-content/uploads/2023/06/EyebrowDetailing.jpg",
+            price: "$10.00",
+            description:"EYEBROW TRIM"
+        },        
+        {
+            name:"ALL-SCISSOR LAYERED HAIRCUT",
+            img:"https://thespotbarbershop.com/wp-content/uploads/2024/10/barber-cutting-hair-with-only-scissors.jpg",
+            price: "$20.00",
+            description:"EYALL-SHEAR HAIRCUT ADDING TEXTURE AND A TAILORED LENGTH, WITH NOT CLIPPERS INVOLVED"
+        },
+        {
+            name:"EXECUTIVE PACKAGE",
+            img:"https://thespotbarbershop.com/wp-content/uploads/2023/06/ExecutivePackage.jpg",
+            price: "$120.00",
+            description:"1 HOUR EXPERIENCE | HAIRCUT HOT TOWEL SHAVE | EAR/NOSE WAX ROYAL MASK | BEARD TRIMMING SCULPTING / STYLING"
+        },
+    ]
     return (
         <>
             <div className="border-4 bg-black border-purple-500  min-h-[200vh]">
@@ -21,17 +84,19 @@ function ListServices() {
                     </div>
 
                     {/* Title */}
-                    <h2 className="text-4xl md:text-5xl font-bold tracking-wider">
+                    <h2 data-aos="zoom-in"
+                        data-aos-duration="3000"
+                        className="mt-[40px] text-3xl text-[#fff] md:text-3xl font-bold tracking-wider">
                         OUR PREMIUM SERVICES
                     </h2>
 
                     {/* Decorative Divider */}
                     <div className="flex items-center justify-center gap-4 py-4">
-                        <div   data-aos="fade-left"  data-aos-duration="2000"
-                        className="rounded-lg h-[6px] w-[150px] bg-white"></div>
+                        <div data-aos="fade-left" data-aos-duration="2000"
+                            className="rounded-lg h-[6px] w-[150px] bg-white"></div>
                         <div className="animate-spin [animation-delay:.7s] w-[20px] h-[20px]  bg-[#CD7F32]"></div>
-                        <div  data-aos="fade-right"  data-aos-duration="2000"
-                        className="rounded-lg h-[6px] w-[150px] bg-white"></div>
+                        <div data-aos="fade-right" data-aos-duration="2000"
+                            className="rounded-lg h-[6px] w-[150px] bg-white"></div>
                     </div>
 
                     {/* Description */}
@@ -40,6 +105,51 @@ function ListServices() {
                         and massage are <span className="font-semibold">included with all services.</span>
                     </p>
                 </div>
+
+                <div className='flex justify-center items-center  gap-4 flex-wrap'>
+
+
+                
+
+                {servicios.map((servicio, index) => (
+                    <div
+                        class="mt-[100px] card shadow-[0px_4px_16px_px_#367E08] h-[400px] w-[280px] group gap-[0.5em] rounded-[1.5em] relative flex justify-end flex-col p-[1.5em] z-[1] overflow-hidden"
+                    >
+                        <div class="absolute top-0 left-0 h-full w-full bg-[#111111]">
+
+                            <img src={servicio.img} alt="" />
+
+                        </div>
+
+                        <div
+                            class="container text-white z-[2] relative font-nunito flex flex-col gap-[0.5em]"
+                        >
+
+
+                            <div class="flex justify-center items-center h-fit w-fit gap-[0.5em]">
+                                <div
+                                    class="border-2 border-white rounded-[0.5em] text-[#000] bg-white font-nunito text-[1em] font-normal px-[0.5em] py-[0.05em] hover:bg-transparent hover:text-[#fff] duration-300 cursor-pointer"
+                                >
+                                    <p>
+                                        {servicio.name}
+                                    </p>
+                                </div>
+                                <div
+                                    class="border-2 border-white rounded-[0.5em] text-[#000] bg-white font-nunito text-[1em] font-normal px-[0.5em] py-[0.05em] hover:bg-transparent hover:text-[#fff] duration-300 cursor-pointer"
+                                >
+                                    <p>{servicio.price}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <p
+                            class=" font-semibold block text-white  relative h-[0em] group-hover:h-[7em] leading-[1.2em] duration-500 overflow-hidden"
+                        >
+                                {servicio.description}
+                        </p>
+                    </div>
+                    ))}
+                </div>
+
             </div>
         </>
     )
